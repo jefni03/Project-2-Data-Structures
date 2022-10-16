6f29c314b8c9fa94c1e3f4490f6eaedfeb52eab8
Justin Nguyen <justinnguyen2245@gmail.com>
2022-10-15 19:24:21 -0700

    interface methods

--- StackInterface.java
+++ StackInterface.java
@@ -1,3 +1,29 @@
-public class StackInterface {
-	
+public interface StackInterface<T>{
+
+	/**
+	 * Adds a new entry to the top of the stack.
+	 * @param newEntry
+	 */
+	public void push(T newEntry);
+
+	/**
+	 * Removes an entry from the top of the stack and returns it.
+	 * @return entry from the top of stack
+	 */
+	public T pop();
+
+	/**
+	 * @return entry from the top of stack
+	 */
+	public T peek();
+
+	/**
+	 * @return if stack is empty
+	 */
+	public boolean isEmpty();
+
+	/**
+	 * Removes all entries from the stack.
+	 */
+	public void clear();
 }
