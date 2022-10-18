772fcf3345689fc5d95140ff2f3cf4ad44c43bfa
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-17 17:13:56 -0700

    first commit

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,3 +1,16 @@
-public class LinkedStack {
-	
+public class LinkedStack<T> implements StackInterface<T>
+{
+     private Node topNode;
+
+     public LinkedStack()
+     {
+          topNode = null;
+     }
+
+     private class Node
+     {
+          private T data;
+          private Node next;
+          
+     }
 }
