671c51791a86bf1ee3e740dd549187e286c63ce6
ppeternnguyen <89424139+2SikNinja@users.noreply.github.com>
2022-10-17 18:57:54 -0700

    linked stack finished?

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,3 +1,6 @@
+import java.util.EmptyStackException;
+import java.util.*;
+
 public class LinkedStack<T> implements StackInterface<T>
 {
      private Node topNode;
@@ -11,6 +14,83 @@ public class LinkedStack<T> implements StackInterface<T>
      {
           private T data;
           private Node next;
-          
+
+          private Node getNextNode()
+          {
+               return next;
+          }
+
+          private void setNextNode(Node next)
+          {
+               this.next=next;
+          }
+
+          private T getData(){
+               return data;
+           }
+
+          private void setData(T data)
+          {
+               this.data=data;
+          }
+           
+   
+   
+
      }
+
+     /**
+	 * Adds an entry to the top of the stack array.
+	 * Checks the integrity of object and if there is space to add beforehand.
+	 * @param newEntry entry to add to top of stack
+	 */
+	public void push(T newEntry)
+     {
+		Node newNode = new Node(newEntry, topNode);
+          topNode = newNode;
+	}
+
+	/**
+	 * Removes the top entry of the stack and returns it.
+	 * @return the removed entry from stack
+	 */
+	public T pop()
+     {
+          T top = peek();
+          topNode = topNode.getNextNode();
+
+          return top;
+	}
+
+	/**
+	 * @return top entry of the stack
+	 */
+	public T peek()
+     {
+		if(isEmpty())
+          {
+               throw new EmptyStackException();
+               
+          }
+          else
+          {
+               return topNode.getData();
+          }
+	}
+
+	/**
+	 * @return if stack array has no entries
+	 */
+	public boolean isEmpty()
+     {
+		return topNode == null;
+	}
+
+	/**
+	 * Sets all indexes in stack array to null
+	 */
+	public void clear()
+     {
+          topNode = null;
+	}
 }
